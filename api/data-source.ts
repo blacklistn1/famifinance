47c6e86fbd72b3e972d3aa9cffa17d6bbd6e6387
Kevin Tran <kevin.tran@vmodev.com>
2022-12-20 15:04:00 +0700

    Init for backend

--- api/data-source.ts
+++ api/data-source.ts
@@ -1,4 +1,35 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
 const dataSourceOptions = {
-  type: 'mysql',
+  synchronize: false,
+  dropSchema: false,
   database: 'famifinance',
 };
+
+if (['development', 'production'].includes(process.env.NODE_ENV)) {
+  Object.assign(dataSourceOptions, {
+    type: 'mariadb',
+    host: 'localhost',
+    username: 'root',
+    password: '123123123',
+    entities: ['src/**/*.entity.js'],
+  });
+}
+
+if (['testing'].includes(process.env.NODE_ENV)) {
+  Object.assign(dataSourceOptions, {
+    type: 'sqlite',
+    database: ':memory:',
+    dropSchema: true,
+    entities: ['src/**/*.entity.ts'],
+  });
+}
+
+const typeDS = JSON.parse(JSON.stringify(dataSourceOptions));
+Object.assign(typeDS, {
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+});
+const dataSource = new DataSource(typeDS as DataSourceOptions);
+
+export { dataSource, dataSourceOptions };
